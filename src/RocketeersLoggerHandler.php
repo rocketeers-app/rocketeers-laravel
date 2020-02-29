43fb599edd6c0b02fc2f9090b20ba8c7f230fd0c
Mark van Eijk <mark@vaneijk.co>
2020-02-29 19:31:55 +0100

    add data

--- src/RocketeersLoggerHandler.php
+++ src/RocketeersLoggerHandler.php
@@ -2,6 +2,7 @@
 
 namespace Rocketeers\Laravel;
 
+use Illuminate\Http\Request;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
 use Rocketeers\Rocketeers;
@@ -9,10 +10,12 @@ use Rocketeers\Rocketeers;
 class RocketeersLoggerHandler extends AbstractProcessingHandler
 {
     protected $client;
+    protected $request;
 
     public function __construct(Rocketeers $client, $level = Logger::DEBUG, $bubble = true)
     {
         $this->client = $client;
+        $this->request = Request::createFromGlobals();
 
         parent::__construct($level, $bubble);
     }
@@ -22,11 +25,16 @@ class RocketeersLoggerHandler extends AbstractProcessingHandler
         $this->client->report([
             'message' => $report['message'],
             'context' => $report['context'],
+            'exception' => $report['context']['exception'],
             'level' => $report['level'],
             'level_name' => $report['level_name'],
             'channel' => $report['channel'],
             'datetime' => $report['datetime'],
             'extra' => $report['extra'],
+            'url' => $this->request->getUri(),
+            'ip_address' => $this->request->getClientIp(),
+            'method' => $this->request->getMethod(),
+            'user_agent' => $this->request->headers->get('User-Agent'),
         ]);
 
         return;
